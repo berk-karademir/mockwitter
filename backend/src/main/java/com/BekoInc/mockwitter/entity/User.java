775f89e8eeabb6c6ca791bc6da64a09f48922673
Berk KARADEMİR <berk.karademir@hotmail.com>
2025-03-03 11:57:26 +0300

    improvements for validations of User fields

--- backend/src/main/java/com/BekoInc/mockwitter/entity/User.java
+++ backend/src/main/java/com/BekoInc/mockwitter/entity/User.java
@@ -1,12 +1,14 @@
 package com.BekoInc.mockwitter.entity;
 
 
+import com.BekoInc.mockwitter.util.Regexes;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -36,23 +38,35 @@ public class User {
     @Size(max = 24, message = "Last name should include 24 characters maximum!")
     private String lastName;
 
-
-    @Pattern(regexp = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$")
-    @Email(message = "Email format is not valid!")
+    @Pattern(regexp = Regexes.EMAIL_REGEX , message = "Email is not valid!")
+    @Email(message = "Email is not valid!")
     @NotNull(message = "Email can not be null!")
     @NotBlank(message = "Email can not be empty!")
     private String email;
 
+    @Pattern(regexp = Regexes.USERNAME_REGEX, message = "Username is not valid!")
     @Column(name = "username", unique = true, nullable = false)
     @NotNull(message = "Username can not be empty!")
     @NotBlank(message = "Username can not be blank!")
-    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters!")
+    @Size(min = 3, max = 20, message = "Username can not include special characters and must be between 3 and 20 characters!")
     private String userName;
 
-    @Pattern(regexp="(^$|[0-9]{10})")
+
+//    At least one digit.
+//    At least one lowercase letter.
+//    At least one uppercase letter.
+//    At least one special character.
+//    No whitespace allowed.
+//    Length between 8 and 20 characters.
+    @Column(name = "password", nullable = false)
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = Regexes.PASSWORD_REGEX, message = "Password must be 8-20 characters long, include at least one uppercase letter, one lowercase letter, one digit, one special character and white space is not allowed!")
+    private String password;
+
+    @Pattern(regexp= Regexes.PHONE_NUMBER_REGEX, message = "Phone number is not valid!")
     @Column(name = "phone_number")
     private String phoneNumber;
-
 
     @CreationTimestamp
     @Column(name = "registration_date")
---
+++ backend/src/main/java/com/BekoInc/mockwitter/util/Regexes.java
@@ -0,0 +1,13 @@
+package com.BekoInc.mockwitter.util;
+
+
+public class Regexes {
+
+    public static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+
+    public static final String USERNAME_REGEX = "^[A-Za-z0-9]{1,20}$";
+
+    public static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=\\S+$).{8,20}$";
+
+    public static final String PHONE_NUMBER_REGEX = "^(\\+\\d{1,3})?[0-9]{10}$";
+}
